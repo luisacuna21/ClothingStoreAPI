6f862ed83d79f676305e5dcf033d7711b81f434e
Luis Acuña <acunaluis560@gmail.com>
2022-03-04 16:17:42 -0600

    fourth commit

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,15 +8,15 @@ builder.Services.Configure<ClothingStoreDBSettings>(builder.Configuration.GetSec
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
